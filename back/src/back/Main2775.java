ea921cd0994a717950f3b343421781fc3b24b5d8
KTG <witness0109@gmail.com>
2020-07-01 16:36:44 +0900

    난 부녀회장이 될거야~

--- back/src/back/Main2775.java
+++ back/src/back/Main2775.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Main2775 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int T = sc.nextInt();
-
+		
 		for( int i = 0; i < T; i++) {
-			int k = sc.nextInt();
-			int n = sc.nextInt();
-			
+			int k = sc.nextInt();//Ãþ  
+			int n = sc.nextInt();//È£ 
+			System.out.println(el(k,n));
 		}
+	}
+	
+	private static int el(int k, int n) {
+		if (n == 0) return 0;
+		else if (k == 0) return n;
+		else return el(k,n-1) + el(k-1,n);
 	}
 }
