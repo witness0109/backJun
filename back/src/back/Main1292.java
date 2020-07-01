35474a988e2ce652b7fbde47630f7e63cee24d53
KTG <witness0109@gmail.com>
2020-07-01 17:40:36 +0900

    문제추가

--- back/src/back/Main1292.java
+++ back/src/back/Main1292.java
@@ -9,6 +9,11 @@ public class Main1292 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		/*
+		 * 1을 한 번, 2를 두 번, 3을 세 번, 이런 식으로 1 2 2 3 3 3 4 4 4 4 5 .. 
+		 * 이러한 수열을 만들고 어느 일정한 구간을 주면 그 구간의 합을 구하는 것이다.
+		 */
 
 		int[] arr = new int[1001];
 		int a = Integer.parseInt(st.nextToken());
@@ -17,15 +22,13 @@ public class Main1292 {
 
 		for (int i = 1; i <= 1000; i++) {
 			for (int j = 1; j <= cur; j++) {
-				if (i > 1000)
-					break;
+				if (i > 1000) break;
 				arr[i] = cur;
 				i++;
 			}
 			cur++;
 			i--;
 		}
-
 		int ans = 0;
 		for (int i = a; i <= b; i++) {
 			ans += arr[i];
